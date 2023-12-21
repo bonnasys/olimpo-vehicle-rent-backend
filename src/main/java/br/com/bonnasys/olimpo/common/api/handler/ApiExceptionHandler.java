530a400e9acdc7d5cea4877d0c193d382c95be28
Gabriel Rodrigues <gabriel.srodrigues.006@gmail.com>
2023-12-21 20:36:22 -0300

    adicionando sonar para ver o quê vai acontecer

--- src/main/java/br/com/bonnasys/olimpo/common/api/handler/ApiExceptionHandler.java
+++ src/main/java/br/com/bonnasys/olimpo/common/api/handler/ApiExceptionHandler.java
@@ -1,14 +1,18 @@
 package br.com.bonnasys.olimpo.common.api.handler;
 
 import br.com.bonnasys.olimpo.users.domain.exception.UserEmailAlreadyInUseException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 
-@RestControllerAdvice
+@Slf4j
+@ControllerAdvice
 public class ApiExceptionHandler {
 
     @ExceptionHandler(value = UserEmailAlreadyInUseException.class)
@@ -16,9 +20,27 @@ public class ApiExceptionHandler {
         return ResponseEntity.badRequest().body(ApiError.from(ex));
     }
 
-    public record ApiError(String message, Integer status, OffsetDateTime timestamp, List<Error> errors) {
+    @ExceptionHandler(value = BadCredentialsException.class)
+    public ResponseEntity<ApiError> handleBadCredentialsException(final BadCredentialsException ex) {
+        return ResponseEntity.badRequest().body(ApiError.from(ex));
+    }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    public ResponseEntity<ApiError> handleAccessDeniedException(final AccessDeniedException ex) {
+        return ResponseEntity.badRequest().body(ApiError.from(ex));
+    }
+
+    public record ApiError(String message, Integer status, OffsetDateTime timestamp, List<String> errors) {
         static ApiError from(final UserEmailAlreadyInUseException ex) {
             return new ApiError(ex.getMessage(), 400, OffsetDateTime.now(), List.of());
+        }
+
+        static ApiError from(final BadCredentialsException ex) {
+            return new ApiError("Invalid username or password!", 400, OffsetDateTime.now(), List.of(ex.getMessage()));
+        }
+
+        static ApiError from(final AccessDeniedException ex) {
+            return new ApiError("Access denied", 400, OffsetDateTime.now(), List.of(ex.getMessage()));
         }
     }
 }
